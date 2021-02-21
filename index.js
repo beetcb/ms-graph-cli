e5ac6d48049575cd1bfde63b387d7eca090a4549
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-21 23:41:59 +0800

    feat(i18n): add ZH_CN hints and lang command line options

--- index.js
+++ index.js
@@ -2,66 +2,101 @@
 import { prompt } from 'inquirer'
 import { EOL } from 'os'
 import { writeFileSync } from 'fs'
-import fetch from 'node-fetch'
 import { resolve } from 'path'
+import fetch from 'node-fetch'
 
 const headers = {
   'content-type': 'application/x-www-form-urlencoded',
 }
 
 // Prompt and acquire code, returns credentials
-async function init() {
+async function init(lang) {
+  // Print i18n hints
+  // - 0 EN_US
+  // - 1 ZH_CN
+  const i18nHints = {
+    account_type: [
+      ['Please select your OneDrive or SharePoint account type:', 'Global', 'Operated by 21Vianet in China'],
+      ['请选择你的 OneDrive 或 SharePoint 账户类型:', '国际版', '世纪互联版'],
+    ],
+    deploy_type: [
+      ['Please select your deploy type (OneDrive or SharePoint):', 'OneDrive', 'SharePoint'],
+      ['请选择你的部署类型:', 'OneDrive', 'SharePoint'],
+    ],
+    client_id: ['Enter your client_id:', '请提供你的 client_id:'],
+    client_secret: ['Enter your client_secret:', '请提供你的 client_secret:'],
+    redirect_uri: [
+      'Enter your redirect_uri (Default：http://localhost):',
+      '请提供你的 redirect_uri (默认：http://localhost):',
+    ],
+    goBrowser: [
+      'Use your browser to visit this URL for login and authorization:',
+      '请在浏览器中打开此地址进行登录和授权:',
+    ],
+    backTerminal: [
+      `Then enter the address you were redirected to(it's in your address bar):\n`,
+      ' 然后请输入浏览器地址栏重定向的地址:\n',
+    ],
+  }
+
   let questions = [
     {
       type: 'list',
       name: 'account_type',
-      message: 'Please select your OneDrive or SharePoint account type:',
+      message: i18nHints.account_type[lang][0],
       choices: [
         {
           value: 1,
-          name: 'Global',
+          name: i18nHints.account_type[lang][1],
         },
         {
           value: 0,
-          name: 'Operated by 21Vianet in China',
+          name: i18nHints.account_type[lang][2],
         },
       ],
     },
     {
       type: 'list',
       name: 'deploy_type',
-      message: 'Please select your deploy type (OneDrive or SharePoint):',
+      message: i18nHints.deploy_type[lang][0],
       choices: [
         {
           value: 1,
-          name: 'OneDrive',
+          name: i18nHints.deploy_type[lang][1],
         },
         {
           value: 0,
-          name: 'SharePoint',
+          name: i18nHints.deploy_type[lang][2],
         },
       ],
     },
     {
       type: 'input',
       name: 'client_id',
-      message: 'Enter your client_id:',
+      message: i18nHints.client_id[lang],
     },
     {
       type: 'input',
       name: 'client_secret',
-      message: 'Enter your client_secret:',
+      message: i18nHints.client_secret[lang],
     },
     {
       type: 'input',
       name: 'redirect_uri',
-      message: 'Enter your redirect_uri (http://localhost would be ok here):',
+      message: i18nHints.redirect_uri[lang],
     },
   ]
 
   let res = await prompt(questions)
 
-  const { client_id, client_secret, deploy_type, account_type, redirect_uri } = res
+  const { client_id, client_secret, deploy_type, account_type } = res
+  
+  // We need to manually set it cause iquirer set it as ''
+  // so we can't use destructuring assignment default values
+  let { redirect_uri } = res
+  if (redirect_uri === '') {
+    redirect_uri = 'http://localhost'
+  }
 
   const auth_endpoint = `${
     account_type ? 'https://login.microsoftonline.com' : 'https://login.partner.microsoftonline.cn'
@@ -71,13 +106,13 @@ async function init() {
     {
       type: 'input',
       name: 'code',
-      message: `Use your browser to access this URL:\n${auth_endpoint}/authorize?${new URLSearchParams({
+      message: `${i18nHints.goBrowser[lang]}\n${auth_endpoint}/authorize?${new URLSearchParams({
         client_id,
         scope: deploy_type
           ? 'Files.Read.All Files.ReadWrite.All offline_access'
           : 'Sites.Read.All Sites.ReadWrite.All offline_access',
         response_type: 'code',
-      }).toString()}&redirect_uri=${redirect_uri}\nThen enter the address you were redirected to(it's in your address bar):`,
+      }).toString()}&redirect_uri=${redirect_uri}\n${i18nHints.backTerminal[lang]}`,
     },
   ]
 
@@ -123,24 +158,39 @@ async function acquireToken(credentials) {
   return credentials
 }
 
-async function getDriveApi(credentials) {
+async function getDriveApi(credentials, lang) {
+  // i18n hints
+  // - 0 EN_US
+  // - 1 ZH_CN
+  const i18nHints = {
+    isNeedSiteId: [
+      ['Do you want to get SharePoint SiteId ?', 'YES', 'NO'],
+      ['是否获取 SharePoint SiteId ?', '是', '否'],
+    ],
+    hostName: [
+      'To get the SharePoint SiteID, You must specify:\n1. SharePoint site host (e.g., cent.sharepoint.com)',
+      '为获取 SharePoint Site，你需要提供如下两个参数:\n1. SharePoint site host (比如：cent.sharepoint.com)',
+    ],
+    sitePath: ['SharePoint sites path (e.g., /sites/centUser)', 'SharePoint sites path (比如：/sites/centUser)'],
+  }
+
   const { account_type, deploy_type, access_token } = credentials
   const graphApi = account_type ? 'https://graph.microsoft.com/v1.0' : 'https://microsoftgraph.chinacloudapi.cn/v1.0'
   if (!deploy_type) {
     // SharePoint
-    questions = [
+    let questions = [
       {
         type: 'list',
         name: 'isNeedSiteId',
-        message: 'Do you want to get SharePoint SiteId ?',
+        message: i18nHints.isNeedSiteId[lang][0],
         choices: [
           {
             value: 1,
-            name: 'YES',
+            name: i18nHints.isNeedSiteId[lang][1],
           },
           {
             value: 0,
-            name: 'NO',
+            name: i18nHints.isNeedSiteId[lang][2],
           },
         ],
       },
@@ -151,13 +201,12 @@ async function getDriveApi(credentials) {
         {
           type: 'input',
           name: 'hostName',
-          message:
-            'To get the SharePoint SiteID, You must specify:\n1. SharePoint site host (e.g., cent.sharepoint.com)',
+          message: i18nHints.hostName[lang],
         },
         {
           type: 'input',
           name: 'sitePath',
-          message: 'SharePoint sites path (e.g., /sites/centUser)',
+          message: i18nHints.sitePath[lang],
         },
       ]
       res = await prompt(questions)
@@ -189,12 +238,34 @@ function delKey(credentials) {
 }
 
 ;(async () => {
-  const credentials = await acquireToken(await init())
-  await getDriveApi(credentials)
+  // Command line arguments parser
+  const argumets = process.argv.slice(2)
+  // Default: don't save, lang is EN
+  let [isSave, lang, isLangSpecified] = [0, 0, 0]
+  argumets.forEach((e) => {
+    switch (e) {
+      case '-s':
+      case '--save': {
+        isSave = 1
+        break
+      }
+      case '-l':
+      case '--lang': {
+        isLangSpecified = 1
+        break
+      }
+      case 'CN':
+      case 'EN': {
+        if (e === 'CN' && isLangSpecified) lang = 1
+      }
+    }
+  })
+
+  const credentials = await acquireToken(await init(lang))
+  await getDriveApi(credentials, lang)
   delKey(credentials)
 
-  const save = process.argv[2]
-  if (save) {
+  if (isSave) {
     writeFileSync(
       resolve('./.env'),
       Object.keys(credentials).reduce((env, e) => {
